27a8c120231f2e5123bd24585e41b1f791200715
Mikah Mathews <mikah@outofband.org>
2022-01-21 10:21:27 -0800

    Add function that is able to determine if number contains 1,2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 function beepBoop(num) {
   
   let numNum = parseInt(num);
+  
 
   for (let i = 0; i<=numNum; i++) {
     let strNum = i.toString();
@@ -12,8 +13,14 @@ function beepBoop(num) {
       console.log("Boop");
     } else if (i === 3) {
       console.log("Won't you be my neighbor")
+    } else if (strNum.includes("3")) {
+      console.log("Won't you be my neighbor");
+    } else if (strNum.includes("2")) {
+      console.log("Boop");
+    } else if (strNum.includes("1")) {
+      console.log("Beep");
     } else {
-      console.log(i);
+      console.log(i)
     }
   }
 }
