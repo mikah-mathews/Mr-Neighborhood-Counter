94bfd25ce5b17ffb1f50d70c70717c8b4cf5d60f
Mikah Mathews <mikah@outofband.org>
2022-01-21 10:08:42 -0800

    Add working function that can replace 1,2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 // business logic
 
 function beepBoop(num) {
-  let strNum = num.toString();
+  
   let numNum = parseInt(num);
 
-  for (let i = 0; i<numNum; i++) {
-    console.log(i);
+  for (let i = 0; i<=numNum; i++) {
+    let strNum = i.toString();
+    if (i === 1) {
+      console.log("Beep");
+    } else if (i === 2) {
+      console.log("Boop");
+    } else if (i === 3) {
+      console.log("Won't you be my neighbor")
+    } else {
+      console.log(i);
+    }
   }
-  
 }
