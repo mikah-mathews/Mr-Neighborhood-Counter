a3c8a38603d157a979144bd2da50d899928f0e0e
Mikah Mathews <mikah@outofband.org>
2022-01-21 10:49:01 -0800

    Add function to make reversed array for counting down

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
 // business logic
 
-function beepBoop(num) {
+function beepBoop(num, name) {
   
   let numNum = parseInt(num);
-  
+  let returnArray = [];
 
   for (let i = 0; i<=numNum; i++) {
     let strNum = i.toString();
     if (i === 1) {
-      console.log("Beep");
+      returnArray.push("Beep");
     } else if (i === 2) {
-      console.log("Boop");
+      returnArray.push("Boop");
     } else if (i === 3) {
-      console.log("Won't you be my neighbor")
+      returnArray.push("Won't you be my neighbor, " + name);
     } else if (strNum.includes("3")) {
-      console.log("Won't you be my neighbor");
+      returnArray.push("Won't you be my neighbor, " + name);
     } else if (strNum.includes("2")) {
-      console.log("Boop");
+      returnArray.push("Boop");
     } else if (strNum.includes("1")) {
-      console.log("Beep");
+      returnArray.push("Beep");
     } else {
-      console.log(i)
+      returnArray.push(i);
     }
   }
+  return returnArray;
+}
+
+function reversed(array) {
+  let reverseArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reverseArray.push(array[i]);
+  }
+  return reverseArray;
 }
